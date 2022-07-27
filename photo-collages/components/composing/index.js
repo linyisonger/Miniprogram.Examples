c84fa950456da68549215f9ba62f0c3af5993b48
linyisonger <752957127@qq.com>
2022-07-27 22:36:00 +0800

    feat: 增加类型

--- photo-collages/components/composing/index.js
+++ photo-collages/components/composing/index.js
@@ -30,14 +30,16 @@ const papers = [
   new PaperSize(600, 900, '6'),
   new PaperSize(686, 457, '6R')
 ]
-/** @type { {count:number,paperSize:PaperSize,composing:Composing[]}[] } */
-const config = []
+/** @type { {count:number,paperSize:PaperSize,weight:number,composing:Composing[]}[] } */
+let config = []
 
 papers.forEach(ps => {
+  let tmp = []
   if (ps.n.endsWith('R')) {
-    config.push(...[
+    tmp = [
       {
         count: 1,
+        icon: '/images/12.png',
         paperSize: ps,
         composing: [
           new Composing(0, 0, 1, 1),
@@ -143,12 +145,123 @@ papers.forEach(ps => {
           new Composing(2 / 3, 2 / 3, 1 / 3, 1 / 3),
         ]
       },
-    ])
+    ]
+    config = config.concat(tmp.map(a => {
+      a.weight = 2;
+      return a
+    }))
+    tmp = [
+      {
+        count: 2,
+        icon: '/images/24.png',
+        paperSize: ps,
+        composing: [
+          new Composing(0, 0, .5, 2 / 3),
+          new Composing(.5, 1 / 3, .5, 2 / 3),
+        ]
+      },
+      {
+        count: 3,
+        icon: '/images/34.png',
+        paperSize: ps,
+        composing: [
+          new Composing(0, 0, 2 / 3, 1),
+          new Composing(2 / 3, 0, 1 / 3, .5),
+          new Composing(2 / 3, .5, 1 / 3, .5),
+        ]
+      },
+      {
+        count: 4,
+        icon: '/images/44.png',
+        paperSize: ps,
+        composing: [
+          new Composing(0, 0, 1, .5),
+          new Composing(0, .5, 1 / 3, .5),
+          new Composing(1 / 3, .5, 1 / 3, .5),
+          new Composing(2 / 3, .5, 1 / 3, .5),
+        ]
+      },
+      {
+        count: 5,
+        icon: '/images/54.png',
+        paperSize: ps,
+        composing: [
+          new Composing(0, 0, 3 / 5, .5),
+          new Composing(0, .5, 3 / 5, .5),
+          new Composing(3 / 5, 0, 2 / 5, 1 / 3),
+          new Composing(3 / 5, 1 / 3, 2 / 5, 1 / 3),
+          new Composing(3 / 5, 2 / 3, 2 / 5, 1 / 3),
+        ]
+      },
+      {
+        count: 6,
+        icon: '/images/64.png',
+        paperSize: ps,
+        composing: [
+          new Composing(0, 0, 1 / 3, 2 / 5),
+          new Composing(1 / 3, 0, 1 / 3, 2 / 5),
+          new Composing(2 / 3, 0, 1 / 3, 2 / 5),
+          new Composing(0, 2 / 5, 1 / 3, 3 / 5),
+          new Composing(1 / 3, 2 / 5, 1 / 3, 3 / 5),
+          new Composing(2 / 3, 2 / 5, 1 / 3, 3 / 5),
+        ]
+      },
+      {
+        count: 7,
+        icon: '/images/74.png',
+        paperSize: ps,
+        composing: [
+          new Composing(0, 0, .5, 1 / 3),
+          new Composing(.5, 0, .5, 1 / 3),
+          new Composing(0, 1 / 3, .5, 1 / 3),
+          new Composing(.5, 1 / 3, .5, 1 / 3),
+          new Composing(0, 2 / 3, 1 / 3, 1 / 3),
+          new Composing(1 / 3, 2 / 3, 1 / 3, 1 / 3),
+          new Composing(2 / 3, 2 / 3, 1 / 3, 1 / 3),
+        ]
+      },
+      {
+        count: 8,
+        icon: '/images/84.png',
+        paperSize: ps,
+        composing: [
+          new Composing(0, 0, .25, .5),
+          new Composing(0, .5, .25, .5),
+          new Composing(.25, 0, .25, .5),
+          new Composing(.25, .5, .25, .5),
+          new Composing(.5, 0, .25, .5),
+          new Composing(.5, .5, .25, .5),
+          new Composing(.75, 0, .25, .5),
+          new Composing(.75, .5, .25, .5),
+        ]
+      },
+      {
+        count: 9,
+        icon: '/images/94.png',
+        paperSize: ps,
+        composing: [
+          new Composing(0, 0, 1 / 3, 1),
+          new Composing(1 / 3, 0, 1 / 3, .25),
+          new Composing(1 / 3, .25, 1 / 3, .25),
+          new Composing(1 / 3, .5, 1 / 3, .25),
+          new Composing(1 / 3, .75, 1 / 3, .25),
+          new Composing(2 / 3, 0, 1 / 3, .25),
+          new Composing(2 / 3, .25, 1 / 3, .25),
+          new Composing(2 / 3, .5, 1 / 3, .25),
+          new Composing(2 / 3, .75, 1 / 3, .25),
+        ]
+      },
+    ]
+    config = config.concat(tmp.map(a => {
+      a.weight = 4;
+      return a
+    }))
   }
   else {
-    config.push(...[
+    tmp = [
       {
         count: 1,
+        icon: '/images/11.png',
         paperSize: ps,
         composing: [
           new Composing(0, 0, 1, 1),
@@ -254,10 +367,120 @@ papers.forEach(ps => {
           new Composing(2 / 3, 2 / 3, 1 / 3, 1 / 3),
         ]
       },
-    ])
+    ]
+    config = config.concat(tmp.map(a => {
+      a.weight = 1;
+      return a
+    }))
+    tmp = [
+      {
+        count: 2,
+        icon: '/images/23.png',
+        paperSize: ps,
+        composing: [
+          new Composing(0, 0, 2 / 3, .5),
+          new Composing(1 / 3, .5, 2 / 3, .5),
+        ]
+      },
+      {
+        count: 3,
+        icon: '/images/33.png',
+        paperSize: ps,
+        composing: [
+          new Composing(0, 0, 1, 2 / 3),
+          new Composing(0, 2 / 3, .5, 1 / 3),
+          new Composing(0.5, 2 / 3, .5, 1 / 3),
+        ]
+      },
+      {
+        count: 4,
+        icon: '/images/43.png',
+        paperSize: ps,
+        composing: [
+          new Composing(0, 0, .5, 1),
+          new Composing(.5, 0, .5, 1 / 3),
+          new Composing(.5, 1 / 3, .5, 1 / 3),
+          new Composing(.5, 2 / 3, .5, 1 / 3),
+        ]
+      },
+      {
+        count: 5,
+        icon: '/images/53.png',
+        paperSize: ps,
+        composing: [
+          new Composing(0, 0, .5, 3 / 5),
+          new Composing(.5, 0, .5, 3 / 5),
+          new Composing(0, 3 / 5, 1 / 3, 2 / 5),
+          new Composing(1 / 3, 3 / 5, 1 / 3, 2 / 5),
+          new Composing(2 / 3, 3 / 5, 1 / 3, 2 / 5),
+        ]
+      },
+      {
+        count: 6,
+        icon: '/images/63.png',
+        paperSize: ps,
+        composing: [
+          new Composing(0, 0, 3 / 5, 1 / 3),
+          new Composing(0, 1 / 3, 3 / 5, 1 / 3),
+          new Composing(0, 2 / 3, 3 / 5, 1 / 3),
+          new Composing(3 / 5, 0, 2 / 5, 1 / 3),
+          new Composing(3 / 5, 1 / 3, 2 / 5, 1 / 3),
+          new Composing(3 / 5, 2 / 3, 2 / 5, 1 / 3),
+        ]
+      },
+      {
+        count: 7,
+        icon: '/images/73.png',
+        paperSize: ps,
+        composing: [
+          new Composing(0, 0, 1 / 3, .5),
+          new Composing(0, .5, 1 / 3, .5),
+          new Composing(1 / 3, 0, 1 / 3, .5),
+          new Composing(1 / 3, .5, 1 / 3, .5),
+          new Composing(2 / 3, 0, 1 / 3, 1 / 3),
+          new Composing(2 / 3, 1 / 3, 1 / 3, 1 / 3),
+          new Composing(2 / 3, 2 / 3, 1 / 3, 1 / 3),
+        ]
+      },
+      {
+        count: 8,
+        icon: '/images/83.png',
+        paperSize: ps,
+        composing: [
+          new Composing(0, 0, .25, .5),
+          new Composing(0, .5, .25, .5),
+          new Composing(.25, 0, .25, .5),
+          new Composing(.25, .5, .25, .5),
+          new Composing(.5, 0, .25, .5),
+          new Composing(.5, .5, .25, .5),
+          new Composing(.75, 0, .25, .5),
+          new Composing(.75, .5, .25, .5),
+        ]
+      },
+      {
+        count: 9,
+        icon: '/images/93.png',
+        paperSize: ps,
+        composing: [
+          new Composing(0, 0, 1, 1 / 3),
+          new Composing(0, 1 / 3, .25, 1 / 3),
+          new Composing(.25, 1 / 3, .25, 1 / 3),
+          new Composing(.5, 1 / 3, .25, 1 / 3),
+          new Composing(.75, 1 / 3, .25, 1 / 3),
+          new Composing(0, 2 / 3, .25, 1 / 3),
+          new Composing(.25, 2 / 3, .25, 1 / 3),
+          new Composing(.5, 2 / 3, .25, 1 / 3),
+          new Composing(.75, 2 / 3, .25, 1 / 3),
+        ]
+      },
+    ]
+    config = config.concat(tmp.map(a => {
+      a.weight = 3;
+      return a
+    }))
   }
 })
-
+config.sort((a, b) => a.weight - b.weight)
 console.log(config);
 /**
  * @author 林一怂儿 
